db9a808b7a11c59c0be4273faf83d127f17c18fa
ahmedraza <ahmed.raza@10xengineers.ai>
2024-06-25 23:05:49 +0500

    Exc4 of lab01 has been implemented. I have implemented algorithm for detection of cyclic linkedlist

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,28 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
+int ll_has_cycle(node *head)
+{
     /* TODO: Implement ll_has_cycle */
+    // Initialize two pointers: slow and fast
+    node *slow_ptr = head;
+    node *fast_ptr = head;
+
+    // Traverse the list with two pointers
+    while (fast_ptr != NULL && fast_ptr->next != NULL)
+    {
+        // Move slow pointer by one step
+        slow_ptr = slow_ptr->next;
+        // Move fast pointer by two steps
+        fast_ptr = fast_ptr->next->next;
+
+        // If the two pointers meet, there is a cycle
+        if (slow_ptr == fast_ptr)
+        {
+            return 1; // Return true to indicate cycle detected
+        }
+    }
+
+    // If we reach here, it means there is no cycle
+    return 0; // Return false
 }
